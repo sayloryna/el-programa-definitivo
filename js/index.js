2ada65ccfd2f7eeab174ddfd89ebd09f426ef284
sayloryna <clarafdezfraga@gmail.com>
2024-03-09 13:33:50 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const userName = "sayloryna";
+const password = "18Pamplinas";
+const repeatPassword = "18Pamplinas";
+const userAge = "18";
+
+const isLargerThanFive = userName.length >= 5;
+const isLargerThanEight = password.length >= 8;
+const hasNumber = password.match(/[0-9]/) != null;
+const hasUpperLetter = password.match(/[A-Z]/) != null;
+const passwordIsValid = isLargerThanEight && hasNumber && hasUpperLetter;
+const isIdem = password === repeatPassword;
+const isAdult = userAge >= 18;
+
+console.log(`El nombre de usuario es válido: ${isLargerThanFive}`);
+console.log(`La contraseña es válida: ${passwordIsValid}`);
+console.log(`La repetición de la contraseña es válida: ${isIdem}`);
+console.log(`Es mayor de edad: ${isAdult}`);
