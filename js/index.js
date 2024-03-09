ce1108d7e29e97520c6245c4e732327658c5be8b
sayloryna <128187875+sayloryna@users.noreply.github.com>
2024-03-09 17:25:23 +0100

    Update

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ const userAge = 18;
 
 const isLargerThanFive = userName.length >= 5;
 const isLargerThanEight = password.length >= 8;
-const hasNumber = password.match(/[0-9]/) != null;
-const hasUpperLetter = password.match(/[A-Z]/) != null;
+const hasNumber = password.match(/[0-9]/) !== null;
+const hasUpperLetter = password.match(/[A-Z]/) !== null;
 const passwordIsValid = isLargerThanEight && hasNumber && hasUpperLetter;
 const isIdem = password === repeatPassword;
 const isAdult = userAge >= 18;
